4e2e3f2f8a8fcb18b7580fb24bed347d69d3eadd
bradlipovsky <bradlipovsky@gmail.com>
2021-10-27 09:59:38 -0400

    Increased resolution near crevasse. This seems to have solved some long-standing bugs.

--- run_crevasse_simulations.py
+++ run_crevasse_simulations.py
@@ -1,8 +1,3 @@
-# The following few lines of code are unethical and shouldn't be used every again. But they work.
-# import os
-# os.environ['PATH'] = "/home/bradlipovsky/anaconda3/envs/fenicsproject/bin:" + os.environ['PATH']
-# print(os.environ['PATH'])
-
 from time import perf_counter
 import numpy as np
 import pickle
@@ -13,7 +8,6 @@ reload(c)
 
 # print (dolfin.__version__)
 
-
 def main():
     # Geometry: domain width, domain height,  crevasse location, crevasse width, crevasse height
     geom = {'W':60000,'H':300,'Lc':15, 'Wc':1, 'Hc': 5}
@@ -23,14 +17,13 @@ def main():
 
     run_names = ('bottom','surface')
 
-    D = mats['E']/(1-mats['nu']**2) * geom['H']**3 / 12
-    flexural_gravity_wavelength = 2*np.pi*(D/(mats['rhow']*mats['g']))**(1/4)
+    D,flexural_gravity_wavelength= c.fgl(mats,geom)
 
     # Lcs_swell = Lcs
-    Lcs_swell = np.linspace(10,3*flexural_gravity_wavelength,1000)
+    Lcs_swell = np.linspace(10,4*flexural_gravity_wavelength,10000)
     # Lcs_swell = (100,200,300,600,1200,2400,4800,9600,19200)
 
-    filename='swell-sifs-med-res-linear.pkl' 
+    filename='swell-sifs-hi-res-linear.pkl' 
 
     if path.exists(filename):
         print('The output filename has already been used. \n\
@@ -44,7 +37,7 @@ def main():
         for this_run in run_names:
                 print ('Running simulation with %s crevasses in Mode-%s.'%(this_run,mode))
                 t1_start = perf_counter() 
-                output.append(c.call_pmap(geom,mats,this_run,mode,Lcs_swell,96,verbose=True))
+                output.append(c.call_pmap(geom,mats,this_run,mode,Lcs_swell,96,verbose=False))
                 t1_stop = perf_counter()    
                 print("Elapsed time in outer loop: %f s."%(t1_stop-t1_start))
 
