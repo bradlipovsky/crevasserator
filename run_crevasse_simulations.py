ea33d693cfbcbdd882a7cedd0e8b05b07c20c41f
bradlipovsky <bradlipovsky@gmail.com>
2021-11-22 20:07:45 -0500

    swell parameterization actually works now

--- run_crevasse_simulations.py
+++ run_crevasse_simulations.py
@@ -9,6 +9,8 @@ reload(c)
 # print (dolfin.__version__)
 
 def main():
+    max_or_min = 'max'
+    
     # Geometry: domain width, domain height,  crevasse location, crevasse width, crevasse height
     geom = {'W':60000,'H':300,'Lc':15, 'Wc':1, 'Hc': 5}
 
@@ -17,13 +19,13 @@ def main():
 
     run_names = ('bottom','surface')
 
-    D,flexural_gravity_wavelength= c.fgl(mats,geom)
+    D,flexural_gravity_wavelength, lam= c.fgl(mats,geom)
 
     # Lcs_swell = Lcs
     Lcs_swell = np.linspace(10,2*flexural_gravity_wavelength,1000)
     # Lcs_swell = (100,200,300,600,1200,2400,4800,9600,19200)
 
-    filename='swell-sifs-med-res-linear.pkl' 
+    filename='swell-sifs-med-res-linear-max.pkl' 
 
     if path.exists(filename):
         print('The output filename has already been used. \n\
@@ -36,9 +38,12 @@ def main():
     for mode in ('I','II'):
         for this_run in run_names:
                 print ('Running simulation with %s crevasses in Mode-%s.'%(this_run,mode))
+
                 t1_start = perf_counter() 
-                output.append(c.call_pmap(geom,mats,this_run,mode,Lcs_swell,96,verbose=False))
+                output.append(c.call_pmap(geom,mats,this_run,mode,Lcs_swell,96,extrema=max_or_min,
+                                         verbose=False))
                 t1_stop = perf_counter()    
+                
                 print("Elapsed time in outer loop: %f s."%(t1_stop-t1_start))
 
     with open(filename,'wb') as f:
--- run_single_model.py
+++ run_single_model.py
@@ -1,21 +1,22 @@
-import pickle
 import crevasse as c # main module for this project
+from dolfin import *
 
 def main():
 	# Geometry: domain width, domain height,  
 	# crevasse location, crevasse width, crevasse height
-	geom = {'W':60000,'H':300,'Lc':15, 'Wc':1, 'Hc': 5}
+    geom = {'W':60000,'H':300,'Lc':15, 'Wc':1, 'Hc': 5}
 
 	# Materials: Youngs modulus, poisson ratio, 
 	#  ice density, water density, gravity
-	mats = {'E':1e10, 'nu':0.3, 'rho':910, 'rhow':1024, 'g':9.81}
+    mats = {'E':1e10, 'nu':0.3, 'rho':910, 'rhow':1024, 'g':9.81}
 
-	output = c.elasticity_solutions(case='full-minus-prestress',
-		verbose=True,crevasse_location="surface",
+    U,mesh = c.elasticity_solutions(case='full-minus-prestress',
+                verbose=True,crevasse_location="surface",
                 geometry=geom,swell_amplitude=0.0, swell_phase=0.0)
-
-	with open('example_output.pkl', 'wb') as file:
-		pickle.dump( output , file)	
+    fFile = HDF5File(MPI.comm_world,"example_output.h5","w") 
+    fFile.write(U,"/f")
+    fFile.close()
+    File("example_mesh.xml") << mesh
 
 if __name__ == "__main__":
-	main()
+    main()
